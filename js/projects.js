48d49970837569f1e3fcb5e152ef56ab6b7c1e17
Jason Mathew <jaymath@engineering.upenn.edu>
2026-07-25 22:31:00 -0700

    removed pic

--- js/projects.js
+++ js/projects.js
@@ -43,7 +43,6 @@ const PORTFOLIO = {
     {
       images: [
         "assets/images/REVX_driversystems.png",
-        "assets/images/rev11-design.jpg",
         "assets/images/pedal_box.jpg",
         "assets/images/REVX_manufacturing.jpg",
         "assets/images/driversystems.png",
